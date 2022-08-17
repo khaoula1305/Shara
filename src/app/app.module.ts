cc9e023c80a79971e52e9cba3df2cee30bf629d9
Khaoula Benchari <khaoulabenchari9@gmail.com>
2022-08-17 07:44:23 +0100

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    HttpClientModule,
+    SharedModule,
+    NgbModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { SharedRoutingModule } from './shared-routing.module';
-import { PricePipe } from './price.pipe';
+import { PricePipe } from './pipes/price.pipe';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [PricePipe],
-  imports: [CommonModule, SharedRoutingModule],
-  exports: [PricePipe],
+  declarations: [PricePipe, NavBarComponent, HeaderComponent, FooterComponent],
+  imports: [CommonModule],
+  exports: [PricePipe, NavBarComponent, HeaderComponent, FooterComponent],
 })
 export class SharedModule {}
